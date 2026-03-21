8affc7c02bf184fa9cfea7b2dc0cd385d7fc1dc2
kase2228 <etelethiopia@gmail.com>
2026-03-21 14:19:37 +0300

    feat: initial Spring Boot product service

---
+++ src/main/java/com/ctbe/kassahun/ProductServiceApplication.java
@@ -0,0 +1,26 @@
+package com.ctbe.kassahun;
+
+import com.ctbe.kassahun.model.Product;
+import com.ctbe.kassahun.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner seedData(ProductRepository repo) {
+		return args -> {
+			repo.save(new Product("Laptop",  1200.00));
+			repo.save(new Product("Monitor",  350.00));
+			repo.save(new Product("Keyboard",  85.00));
+		};
+	}
+}
+
---
+++ src/main/java/com/ctbe/kassahun/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.ctbe.kassahun.repository;
+
+import com.ctbe.kassahun.model.Product; 
+import org.springframework.data.jpa.repository.JpaRepository; 
+import org.springframework.stereotype.Repository; 
+import java.util.List; 
+  
+@Repository 
+public interface ProductRepository extends JpaRepository<Product, Long> { 
+  
+    // Spring Data automatically generates the SQL for this method: 
+    List<Product> findByNameContainingIgnoreCase(String keyword); 
+}
